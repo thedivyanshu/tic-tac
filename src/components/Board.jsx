39e1c314f9b56f80d095991203dfaa92c4b80a29
Divyanshu2308 <divyanshu120@gmail.com>
2023-01-21 16:51:02 +0530

    first commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Square from "./Square";
+const Board = () => {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [isXturn, setIsXturn] = useState(true);
+
+  const checkWinner = () => {
+    const winner = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let logic of winner) {
+      const [a, b, c] = logic;
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+    return false;
+  };
+
+  const isWinner = checkWinner();
+  const handleClick = (index) => {
+    const copyState = [...state];
+    copyState[index] = isXturn ? "X" : "O";
+    setState(copyState);
+    setIsXturn(!isXturn);
+  };
+  return (
+    <div className="board-container">
+      {isWinner ? (
+        <>{isWinner} won the game</>
+      ) : (
+        <>
+          <div class="board-row">
+            <Square onClick={() => handleClick(0)} value={state[0]} />
+            <Square onClick={() => handleClick(1)} value={state[1]} />
+            <Square onClick={() => handleClick(2)} value={state[2]} />
+          </div>
+          <div class="board-row">
+            <Square onClick={() => handleClick(3)} value={state[3]} />
+            <Square onClick={() => handleClick(4)} value={state[4]} />
+            <Square onClick={() => handleClick(5)} value={state[5]} />
+          </div>
+          <div class="board-row">
+            <Square onClick={() => handleClick(6)} value={state[6]} />
+            <Square onClick={() => handleClick(7)} value={state[7]} />
+            <Square onClick={() => handleClick(8)} value={state[8]} />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Square = (props) => {
+  return (
+    <div onClick={props.onClick} className="square">
+      <h5>{props.value}</h5>
+    </div>
+  );
+};
+export default Square;
